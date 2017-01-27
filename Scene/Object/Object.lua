61a40c367bbaabd694b0447da05fcf50f5e0b444
kasshust <kasshust@gmail.com>
2017-01-27 21:13:06 +0900

    first commit

---
+++ Scene/Object/Object.lua
@@ -0,0 +1,33 @@
+
+
+Object = {
+    table = {};
+    new = function(x,y)
+        local obj = instance(Object)
+
+        obj.x = x or 0
+        obj.y = y or 0
+        obj.direction = 0
+
+        table.insert(Object.table,obj)
+        return obj
+    end;
+    update = function(self,dt)
+    end;
+    draw = function(self)
+    end;
+    drawGUI = function(self)
+    end;
+    destroy = function(self)
+        --[[ 親クラス:destroy() を書いてね ]]
+        removeFromTable(self,ObjectTable)
+        removeFromTable(self,Object.table)
+    end;
+}
+
+--[[
+ - 当たり判定検出の例-
+    obj.mouse = HC.circle(400,300,20)
+    self.mouse:moveTo(love.mouse.getPosition())
+    self.mouse:draw("fill")
+]]--
---
+++ Scene/Room/Room.lua
@@ -0,0 +1,18 @@
+Room1 = {
+    new = function()
+        local obj = instance(Room1,Scene)
+            obj.frame = 0
+            obj.name = "stage1"
+            table.insert(ObjectTable,Object.new(100,100))
+        return obj
+    end;
+}
+
+Room2 = {
+    new = function()
+        local obj = instance(Room2,Scene)
+            obj.frame = 0
+            obj.name = "stage2"
+        return obj
+    end;
+}
