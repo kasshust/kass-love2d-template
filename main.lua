afd1a5893727b62a8e6fbcc56cd25f1769da3764
kasshust <kasshust@gmail.com>
2017-01-30 18:57:38 +0900

    "add Tserial"

--- main.lua
+++ main.lua
@@ -8,6 +8,19 @@ function love.load()
     W,H = 640,480
     love.window.setMode(W, H, {resizable=true, minwidth = W, minheight = H})
 
+    clearflag = {}
+    for i = 1 , 20 do
+        table.insert(clearflag,false)
+    end
+
+    if not love.filesystem.exists("save.lua") then
+        love.filesystem.write("save.lua",Tserial.pack(clearflag))
+    else
+        clearflag = nil
+        clearflag = {}
+        clearflag = Tserial.unpack(love.filesystem.read("save.lua"))
+    end
+
     scenemanager = SceneManager:new(Room1.new())
 end
 function love.update(dt)
